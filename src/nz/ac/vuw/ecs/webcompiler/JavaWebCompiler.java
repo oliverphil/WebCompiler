b4273d62d0806534ad1f0256cd5c5a9e507de946
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2020-06-02 11:34:02 +1200

    Add Erroring Lines Info to /Compile Response
    Add an array of the lines with compilation errors to the /compile
    endpoint response.
    Also update webcompiler-frontend submodule to the latest version
    with handling for showing errored lines.

--- src/nz/ac/vuw/ecs/webcompiler/JavaWebCompiler.java
+++ src/nz/ac/vuw/ecs/webcompiler/JavaWebCompiler.java
@@ -8,13 +8,16 @@ import org.apache.http.protocol.HttpRequestHandler;
 import org.apache.http.util.EntityUtils;
 
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
 
 public class JavaWebCompiler implements HttpRequestHandler {
 
@@ -50,17 +53,28 @@ public class JavaWebCompiler implements HttpRequestHandler {
             BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
             System.out.println(stdout.lines().reduce((a, b) -> a + "\n" + b).get());
-            
+
+            Set<String> errorLines = new HashSet<>();
+
             Optional<String> stderrLines = stderr.lines().reduce((a, b) -> a + "\n" + b);
             String result = "";
             if (stderrLines.isEmpty()) {
                 result = "Compilation Successful";
             } else {
                 result = stderrLines.get();
+                String[] lines = result.split("\n");
+                for (String s: lines) {
+                    if (!s.contains(".java:")) continue;
+                    String[] arr = s.split(":");
+                    errorLines.add(arr[1]);
+                }
             }
+
+            JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder(errorLines);
 
             JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
             jsonBuilder.add("compileResult", result);
+            jsonBuilder.add("errorLines", jsonArrayBuilder);
             String jsonResponse = jsonBuilder.build().toString();
 
             response.setStatusCode(HttpStatus.SC_OK);

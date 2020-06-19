415fe6c6bbc44f1a57a3eaf3c0f6fa6421461a8e
Philip Oliver <oliverphil@ecs.vuw.ac.nz>
2020-06-19 15:23:16 +1200

    Refactor Compilation To Use Session Folders
    Create code files for user inputs in a folder with their session key.
    Added handling to ensure class names are correct and update folder
    names to match exactly. Add import lines to the top of source files
    for the challenges which require them.
    Issue #3

--- src/nz/ac/vuw/ecs/webcompiler/JavaWebCompiler.java
+++ src/nz/ac/vuw/ecs/webcompiler/JavaWebCompiler.java
@@ -1,5 +1,6 @@
 package nz.ac.vuw.ecs.webcompiler;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.http.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.message.BasicHttpEntityEnclosingRequest;
@@ -9,11 +10,9 @@ import org.apache.http.util.EntityUtils;
 
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
@@ -34,22 +33,33 @@ public class JavaWebCompiler implements HttpRequestHandler {
     private void post(HttpRequest httpRequest, HttpResponse httpResponse, HttpContext httpContext) throws HttpException, IOException {
         HttpEntity entity = ((BasicHttpEntityEnclosingRequest) httpRequest).getEntity();
         String content = EntityUtils.toString(entity);
-        if (!content.isEmpty()) {
-            compile(content, httpResponse);
+        JsonObject json = Json.createReader(new StringReader(content)).readObject();
+        String code = json.getString("code");
+        String sessionKey = json.getString("sessionKey");
+        String challengeName = json.getString("challengeName");
+        if (!content.isEmpty() && !sessionKey.isEmpty()) {
+            compile(code, sessionKey, challengeName, httpResponse);
         }
     }
 
-    private void compile(String content, HttpResponse response) {
+    private void compile(String content, String sessionKey, String challengeName, HttpResponse response) {
         try {
-            FileWriter writer = new FileWriter("build/TestCompile.java");
-            String fileContent = "public class TestCompile {" + content + "}";
+            File challengeDir = createRequiredFolders(sessionKey, challengeName);
+            File challengeFile = new File(String.format("%s/%s.java", challengeDir.getPath(), challengeName));
+            FileWriter writer = new FileWriter(challengeFile);
+            File imports = new File(String.format("%s/imports.txt", challengeDir.getPath()));
 
+            String fileContent = String.format("public class %s { %s }", challengeName, content);
+            if (imports.exists()) {
+                String importsString = new BufferedReader(new FileReader(imports)).lines().reduce((a, b) -> a + "\n" + b).get();
+                fileContent = String.format("%s\n%s", importsString, fileContent);
+            }
             System.out.println(fileContent);
             writer.write(fileContent);
             writer.close();
 
             ProcessBuilder builder = new ProcessBuilder();
-            Process p = builder.command("jdk-langtools/build/langtools/bin/javac", "build/TestCompile.java").start();
+            Process p = builder.command("jdk-langtools/build/langtools/bin/javac", challengeFile.getPath()).start();
             BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
             System.out.println(stdout.lines().reduce((a, b) -> a + "\n" + b).get());
@@ -86,5 +96,20 @@ public class JavaWebCompiler implements HttpRequestHandler {
         }
 
         response.setStatusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    private File createRequiredFolders(String sessionKey, String challengeName) throws IOException {
+        File buildFolder = new File("build");
+        File sessionFolder = new File("build/" + sessionKey);
+        if (!sessionFolder.exists()) {
+            sessionFolder.mkdir();
+        }
+        File challenges = new File("coding-challenges/challenges");
+        assert challenges.exists();
+
+        FileUtils.copyDirectory(challenges, sessionFolder);
+
+        File challengeFolder = new File(String.format("build/%s/%s", sessionKey, challengeName));
+        return challengeFolder;
     }
 }
